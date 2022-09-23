72893948bd8b1ce8a012cf3e66a41ad607acb536
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-23 20:33:47 +0000

    feat: get all element info in one database call and conditional to get element by atomic number or name/symbol based on whether passed argument is a number or not

--- element.sh
+++ element.sh
@@ -4,16 +4,18 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1;")
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1;")
+  else
+    ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1';")
+  fi
 
   if [[ -z $ELEMENT ]]
   then
     echo "I could not find that element in the database."
   else
-    PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM properties INNER JOIN types USING(type_id) WHERE atomic_number = $1;")
-
-    IFS=',' read ATOMIC_NUMBER SYMBOL NAME <<< $ELEMENT
-    IFS=',' read ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $PROPERTIES
+    IFS=',' read ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $ELEMENT
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   fi
-  
 fi
