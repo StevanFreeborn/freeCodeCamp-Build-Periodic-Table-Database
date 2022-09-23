891bcfd8f86514b183f8f496ec061d3d49b2a2cd
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-23 20:23:16 +0000

    feat: read properties into individual variables

--- element.sh
+++ element.sh
@@ -4,14 +4,16 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1;")
-  PROPERTIES=$($PSQL "SELECT * FROM properties WHERE atomic_number = $1;")
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1;")
 
   if [[ -z $ELEMENT ]]
   then
     echo "I could not find that element in the database."
   else
+    PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM properties INNER JOIN types USING(type_id) WHERE atomic_number = $1;")
+
     IFS=',' read ATOMIC_NUMBER SYMBOL NAME <<< $ELEMENT
+    IFS=',' read ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $PROPERTIES
   fi
   
 fi
