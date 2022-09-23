e1ce54b1b1baf6d98c944dd5ba237c365bcb25c3
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-23 20:13:40 +0000

    feat: added conditional statement to handle when argument is passed or not passed

--- element.sh
+++ element.sh
@@ -1,1 +1,9 @@
-echo "Please provide an element as an argument."
+PSQL="psql --csv --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1;")
+  echo $ELEMENT
+fi
